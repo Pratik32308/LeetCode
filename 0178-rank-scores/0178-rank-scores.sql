1e6b9244e54aa116f71fcd3d1bc64c8d84c6f1f7
Pratik32308 <pratikranwade07@gmail.com>
2026-06-10 19:22:53 +0530

    Time: 384 ms (17.99%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,7 +1,10 @@
 # Write your MySQL query statement below
-SELECT S.score,  COUNT(S2.score) as 'rank'
-from Scores S,
-(SELECT DISTINCT score FROM Scores) S2
-WHERE S.score<=S2.score
-GROUP BY S.ID
-ORDER BY S.score DESC;
+-- SELECT S.score,  COUNT(S2.score) as 'rank'
+-- from Scores S,
+-- (SELECT DISTINCT score FROM Scores) S2
+-- WHERE S.score<=S2.score
+-- GROUP BY S.ID
+-- ORDER BY S.score DESC;
+SELECT score,DENSE_RANK() OVER (ORDER BY score DESC) 'rank'
+FROM Scores
+ORDER BY score DESC;
