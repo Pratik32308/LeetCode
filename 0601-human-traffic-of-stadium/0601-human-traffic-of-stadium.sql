ad1384003dddcbacdc8fec85420247b7b538f93f
Pratik32308 <pratikranwade07@gmail.com>
2026-06-19 13:14:25 +0530

    Time: 365 ms (86.6%), Space: 0B (100%) - LeetHub

---
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT DISTINCT S1.* FROM Stadium S1,Stadium S2,Stadium S3
+WHERE S1.people>=100 AND S2.people>=100 AND S3.people>=100  
+AND 
+(
+    (S1.id-S2.id=1 AND S2.id-S3.id=1)
+    OR (S3.id-S2.id=1 AND S2.id-S1.id=1)
+    OR (S2.id-S1.id=1 AND S1.id-S3.id=1)
+)
+ORDER BY S1.visit_date;
