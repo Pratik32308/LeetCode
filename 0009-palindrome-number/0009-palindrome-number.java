18451b8c35e6e1a148b7ef65e7b83bced910246e
Pratik32308 <pratikranwade07@gmail.com>
2026-06-29 21:30:56 +0530

    Time: 5 ms (79.54%), Space: 46.2 MB (17.15%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x<0){
+            return false;
+        }
+        int rev=0;
+        int pal=x;
+       while(x!=0){
+        int LD=x%10;
+         rev = rev*10 + LD;
+        x=x/10;
+        }
+        return pal==rev;
+    }
+}
