efffdb41e093cbb0eaa2c2ef85076463d36dad4e
Pratik32308 <pratikranwade07@gmail.com>
2026-07-02 21:41:32 +0530

    Time: 538 ms (46.14%), Space: 0B (100%) - LeetHub

---
+++ 1393-capital-gainloss/1393-capital-gainloss.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT stock_name,
+SUM(CASE WHEN operation='Sell' THEN price END)-SUM(CASE WHEN operation='Buy' THEN price END) capital_gain_loss 
+FROM Stocks
+GROUP BY stock_name;
